b5272ad75666dadb58b5f7af7dddb191dd92f524
Ismael Bautista <ismaelbautista6@gmail.com>
2023-04-19 20:58:24 +0200

    first version - added temp notion db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -106,7 +106,7 @@ export default function Home() {
           <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Resource library</h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Time is gold so we collect almost all the info we gather into a database where we can filter out by categories</p>
-            <Link href={''} className='w-full justify-center gap-2 text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2'>
+            <Link href={'https://ismaelb.notion.site/0297480c38c6455f86878d896c1b323e?v=6cd093c80ce1449a9e32a27f96926302'} className='w-full justify-center gap-2 text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2'>
               <svg xmlns="http://www.w3.org/2000/svg" 
                 fill="none" 
                 viewBox="0 0 24 24" 
