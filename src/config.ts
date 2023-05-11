f36607bf289ee8c7a222cf827b6d54a9d4cdfad2
Ismael Bautista <ismaelbautista6@gmail.com>
2023-05-11 17:11:39 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const config: Config = {
         discord: "https://discord.gg/opensense",
         twitter: "https://twitter.com/opensensepw",
         youtube: "https://www.youtube.com/@opensensepw",
-        github: "https://github.com/opnsnse",
+        github: "https://github.com/opensensepw",
         database: "https://lime-trampoline-6f9.notion.site/c36cbfe49eee4c11ad0fbea676cb12d1?v=2b9076e2eaf34139b3a97ae09a4cb472",
         //// blockchain link repo, needs to be migrated to opensense notion
         //database: "https://ismaelb.notion.site/0297480c38c6455f86878d896c1b323e?v=6cd093c80ce1449a9e32a27f96926302"
